1be60e1efae142ac719380133701cf111a4cdb7a
Hector <168874717+ihekusmiles@users.noreply.github.com>
2024-06-05 09:23:36 -0400

    Update scoreboard.py
    Added exceptions and descriptions

--- scoreboard.py
+++ scoreboard.py
@@ -2,17 +2,29 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ('Courier', 17, 'bold')
 
-
+# This class will create the scoreboard which will be stored in a data.txt file
+# The data.txt file will be created automatically if not already found.
+# Additionally, this class will update the scoreboard and reset it as the game loops.
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt") as data:
-            self.highscore = int(data.read())
         self.setpos(0, 270)
         self.color("white")
         self.hideturtle()
+        self.check_text_file()
         self.update_scoreboard()
+
+    def check_text_file(self):
+        try:
+            with open("data.txt") as data:
+                self.highscore = int(data.read())
+        except FileNotFoundError:
+            with open("data.txt", mode="w") as data:
+                data.write("0")
+        finally:
+            with open("data.txt") as data:
+                self.highscore = int(data.read())
 
     def update_scoreboard(self):
         self.clear()
