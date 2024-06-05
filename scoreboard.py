47522e541d09fd1efbb784cb37dbc2c3bae6302b
Hector <168874717+ihekusmiles@users.noreply.github.com>
2024-06-04 20:33:26 -0400

    Add files via upload
    Snake Game with high score tracker. Will do some updates on this soon.

---
+++ scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Courier', 17, 'bold')
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
+        self.setpos(0, 270)
+        self.color("white")
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", False, ALIGNMENT, FONT)
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
