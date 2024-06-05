8b90e6c1a6f562c81beb9603fdfbe17358f27ac3
Hector <168874717+ihekusmiles@users.noreply.github.com>
2024-06-05 09:12:51 -0400

    Update food.py
    Changed a few parameters for the Food class

--- food.py
+++ food.py
@@ -1,14 +1,14 @@
 from turtle import Turtle
 import random
 
-
+# Food class inherits all attributes from Turtle class
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
-        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
+        self.shapesize(stretch_len=.9, stretch_wid=.9)
+        self.color("green")
         self.speed("fastest")
         self.refresh()
 
